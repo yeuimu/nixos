7ff83668b8ac7ea4d4496201f3fd45f7f0a60269
yoyoki <2197651308@qq.com>
2024-08-04 17:24:38 +0800

    improve: Map key for go to next/previous buffer

--- dotfiles/nvim/lua/init/keymaps.lua
+++ dotfiles/nvim/lua/init/keymaps.lua
@@ -15,6 +15,8 @@ map({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
 -- Buffer operation
 map('n', '<C-w>', '<cmd>bd', { desc = 'Delete current buffer', remap = true })
 map('n', 'gt', '<cmd>BufferLinePick', { desc = 'Pick which buffer', remap = true })
+map('n', 'gp', '<cmd>bp', { desc = 'Go to previous buffer', remap = true })
+map('n', 'gn', '<cmd>bn', { desc = 'Go to next buffer', remap = true })
 
 -- Terminal Mappings
 map("t", "<A-x>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
